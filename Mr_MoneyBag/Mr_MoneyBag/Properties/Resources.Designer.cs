b124e662653c85af41321d0664f23c649a5030d5
cmr <cmr99223@126.com>
2019-12-06 15:19:43 +0800

    resource update

--- Mr_MoneyBag/Mr_MoneyBag/Properties/Resources.Designer.cs
+++ Mr_MoneyBag/Mr_MoneyBag/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Mr_MoneyBag.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        public static System.Drawing.Bitmap enemy_0 {
+            get {
+                object obj = ResourceManager.GetObject("enemy_0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         public static System.Drawing.Bitmap money {
             get {
                 object obj = ResourceManager.GetObject("money", resourceCulture);
