d03bec8eef26c9564e5d95851a3241568027884b
haoran wang <w604391451@hotmail.com>
2019-12-06 12:09:04 +0800

    24

--- Mr_MoneyBag/Mr_MoneyBag/Gameboard.cs
+++ Mr_MoneyBag/Mr_MoneyBag/Gameboard.cs
@@ -12,7 +12,7 @@ namespace Mr_MoneyBag
         public const int width=79, height=79;
         public int level;
         public int turn = 0;
-        public int coinsonfloor;
+        public int coinsonfloor=24,newredgen=20,rednoticedist=8,sight=3;
         public GameObject[,] status = new GameObject[height, width];
         public const int initial_x = 50;
         public const int initial_y = 50;
@@ -41,7 +41,10 @@ namespace Mr_MoneyBag
         {
             return height;
         }
-        public Shop GenShop()
-        { return null; }
+        public Shop GenShop(ref int target)
+        { 
+
+            return null; 
+        }
     }
 }
