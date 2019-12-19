3f1409b717b4f6ce14e0f573d7973012523f5f46
cmr <cmr99223@126.com>
2019-12-19 21:33:47 +0800

    Add sound, Jokerman

--- Mr_MoneyBag/Mr_MoneyBag/Form1.cs
+++ Mr_MoneyBag/Mr_MoneyBag/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Mr_MoneyBag
 {
@@ -28,8 +29,9 @@ namespace Mr_MoneyBag
         private float notice_x,notice_y;
         private bool arrow_key_locked = false;//方向键状态
         private double x_position, y_position;//记录动画中盘面位置
+        SoundPlayer sndplayr;
 
-        Font font = new Font("UD Digi Kyokasho NK-B", 24);
+        Font font = new Font("Jokerman", 24);
         Brush money = new SolidBrush(Color.FromArgb(251, 242, 54));
         Brush level = new SolidBrush(Color.FromArgb(255, 40, 40));
         Brush moneylimit = new SolidBrush(Color.FromArgb(209, 163, 164));
@@ -58,6 +60,9 @@ namespace Mr_MoneyBag
             RefreshBoard();//刷新画面
             timerFresh.Interval = framespeed; //timerFresh 控制画面更新
             timerFresh.Start();
+            sndplayr = new SoundPlayer(Properties.Resources.CroatianRhapsody);
+            sndplayr.PlayLooping();
+
         }
         private ValueTuple<double, double> GetXYst()
         {
--- Mr_MoneyBag/Mr_MoneyBag/Properties/Resources.Designer.cs
+++ Mr_MoneyBag/Mr_MoneyBag/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Mr_MoneyBag.Properties {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -97,6 +97,15 @@ namespace Mr_MoneyBag.Properties {
             get {
                 object obj = ResourceManager.GetObject("Coin_Shoot", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似于 System.IO.MemoryStream 的 System.IO.UnmanagedMemoryStream 类型的本地化资源。
+        /// </summary>
+        public static System.IO.UnmanagedMemoryStream CroatianRhapsody {
+            get {
+                return ResourceManager.GetStream("CroatianRhapsody", resourceCulture);
             }
         }
         
